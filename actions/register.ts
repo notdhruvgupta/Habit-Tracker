1484bd2bc33dfd4c0288e7ce0d1889a37c65028b
Dhruv Gupta <dhruv.gupta2711@gmail.com>
2025-09-12 01:18:48 +0530

    registration successful

--- actions/register.ts
+++ actions/register.ts
@@ -1,17 +1,41 @@
 "use server";
 import { RegisterSchema } from '@/schemas';
 import * as z from 'zod'
+import bcrypt from 'bcrypt'
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateValues = RegisterSchema.safeParse(values);
 
-    if(!validateValues) {
+    if(!validateValues.success) {
         return {
             error: "Invalid Credentials"
         }
     }
 
+    const {name, email, password} = validateValues.data;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const foundUser = await getUserByEmail(email);
+
+    if(foundUser) {
+        return {
+            error: "Email already in use!"
+        }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+
     return {
-        success: "Email Sent!"
+        success: "User Created!"
     }
 }
--- components/auth/Message.tsx
+++ components/auth/Message.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Message({ type, label }: { type: string; label: string }) {
     return (
         <div>
-            <div className={`flex text-sm rounded-md py-2 px-3 gap-2 items-center ${type === 'error' ? 'bg-red-400 text-red-950' : 'bg-emerald-300 text-emerald-950'}`}>
+            <div className={`flex text-sm rounded-full py-2 px-3 gap-2 items-center ${type === 'error' ? 'bg-red-300 text-red-900' : 'bg-emerald-300 text-emerald-950'}`}>
                 {type === 'error' && <CircleAlert size={18} />}
                 {type === 'success' && <CircleCheck size={18} />}
                 <p>{label}</p>
